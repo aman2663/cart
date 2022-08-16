018ebaa6f9f72e42512e317f4a1cf6052bccfb03
aman2663 <amankeshari2663@gmail.com>
2022-08-16 18:41:36 +0530

    first commit

---
+++ Desktop/React/cart/src/components/ProductList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Product from "./Product.js";
+
+export default function (props) {
+  return(props.productList.length>0?
+  props.productList.map((product, ind) => {
+    return (
+      <Product
+        product={product}
+        key={ind}
+        incrementQuantity={props.incrementQuantity}
+        decrementQuantity={props.decrementQuantity}
+        index={ind}
+        removeItem={props.removeItem}
+      />
+    );
+  }):<h1>Please add some product in cart</h1>)
+}
